93a9bdbb5c3ae0b25212ff739fa2d747734d4e9e
William Lin <wlin26@yahoo.com>
2018-03-24 14:25:29 -0400

    Changes and finished lesson 9 app

--- Lesson9App/app/src/main/java/me/williamlin/lesson9app/MainActivity.java
+++ Lesson9App/app/src/main/java/me/williamlin/lesson9app/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -17,6 +19,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private Button downloadImageBtn;
+    private Button downloadTextBtn;
     private ImageView imageView;
     private TextView captionText;
 
@@ -28,17 +31,29 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView1);
         captionText = (TextView) findViewById(R.id.text_file);
         downloadImageBtn = (Button) findViewById(R.id.MyButton);
+        downloadTextBtn = (Button) findViewById(R.id.MyButton2);
         downloadImageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startDownload();
+                startImageDownload();
+            }
+        });
+        downloadTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTextDownload();
             }
         });
     }
 
-    protected void startDownload() {
+    protected void startImageDownload() {
         DownloadFileAsync downloader = new DownloadFileAsync();
         downloader.execute("http://www.ottophoto.com/kirlian/kirlian_1/kirlian12.jpg");
+    }
+
+    protected void startTextDownload() {
+        DownloadTextAsync downloader = new DownloadTextAsync();
+        downloader.execute("https://raw.githubusercontent.com/FanciestW/CSCI4547/master/quote.txt");
     }
 
     private class DownloadFileAsync extends AsyncTask<String, Integer, Drawable> {
@@ -47,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Drawable doInBackground(String... params) {
-            // TODO::Complete
             Drawable draw = null;
             try {
                 draw = DownloadDrawable(params[0], "image.jpg");
@@ -61,13 +75,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
-            captionText.setText("Getting Image");
+            Toast.makeText(getApplicationContext(), "Getting image", Toast.LENGTH_SHORT).show();
         }
 
         @Override
         protected void onPostExecute(Drawable result) {
             imageView.setImageDrawable(result);
-            captionText.setText("Got Image");
+            Toast.makeText(getApplicationContext(), "Successfully got image", Toast.LENGTH_SHORT).show();
         }
 
         @Override
@@ -81,5 +95,53 @@ public class MainActivity extends AppCompatActivity {
             return draw;
         }
 
+    }
+
+    private class DownloadTextAsync extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return DownloadText(params[0]);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "Getting caption", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            captionText.setText(result);
+            Toast.makeText(getApplicationContext(), "Successfully got caption", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        String DownloadText(String urlPath) {
+            BufferedInputStream in = null;
+            String result = "";
+
+            try {
+                URL url = new URL(urlPath);
+
+                in = new BufferedInputStream(url.openStream());
+
+                byte data[] = new byte[1024];
+                int count;
+                // we read until we meet the byte -1
+                while((count = in.read(data, 0, 1024)) != -1){
+                    // this converts byte-s into string
+                    String s = new String(data);
+                    result += s;
+                }
+            } catch (Exception e) {
+                Log.d("Error", e.getMessage());
+            }
+
+            return result;
+        }
     }
 }
