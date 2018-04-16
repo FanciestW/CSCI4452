b4e11052871b245579253bdbedc00252aca96b23
William Lin <wlin26@yahoo.com>
2018-04-15 23:59:48 -0400

    Worked on SensorSQL app

--- SensorSQLApp/app/src/main/java/me/williamlin/sensorsqlapp/MainActivity.java
+++ SensorSQLApp/app/src/main/java/me/williamlin/sensorsqlapp/MainActivity.java
@@ -1,6 +1,9 @@
 package me.williamlin.sensorsqlapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,10 +16,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView xvalueTxt;
-    private TextView yvalueTxt;
-    private TextView zvalueTxt;
-    private TextView scrollText;
+    TextView xvalueTxt, yvalueTxt, zvalueTxt, scrollText;
+    float x ,y ,z = 0;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         scrollText = findViewById(R.id.scrollTextView);
 
         scrollText.setMovementMethod(new ScrollingMovementMethod());
+
+        db = openOrCreateDatabase("SensorDB", Context.MODE_PRIVATE, null);
+        db.execSQL("CREATE TABLE IF NOT EXISTS data(x REAL, y REAL, z REAL);");
     }
 
     public void getDataClick(View view) {
@@ -45,7 +50,36 @@ public class MainActivity extends AppCompatActivity {
                 xvalueTxt.setText(Html.fromHtml(values[0] + "m/s<sup>2</sup>"));
                 yvalueTxt.setText(Html.fromHtml(values[1] + "m/s<sup>2</sup>"));
                 zvalueTxt.setText(Html.fromHtml(values[2] + "m/s<sup>2</sup>"));
+
+                x = values[0];
+                y = values[1];
+                z = values[2];
             }
         }
+    }
+
+    public void onInsertClick(View view) {
+        db.execSQL("INSERT INTO data VALUES ('" + x + "," + y + "," + z + ")");
+    }
+
+    public void onDeleteClick(View view) {
+
+    }
+
+    public void onRetrieveAll(View view) {
+        String query = "SELECT * FROM data LIMIT 1000";
+        Cursor c = db.rawQuery(query, null);
+        if(c.getCount() == 0) {
+            scrollText.setText("No Records");
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        while(c.moveToNext()) {
+            buffer.append("x: " + c.getString(0) + "\n");
+            buffer.append("y: " + c.getString(1) + "\n");
+            buffer.append("z: " + c.getString(2) + "\n");
+        }
+        scrollText.setText(buffer.toString());
     }
 }
